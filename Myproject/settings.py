33e186931320eb873f1f311426137205ac52fd6e
Bluelights01 <ayushgoyal855@gmail.com>
2025-10-29 01:07:00 +0530

    newstatic

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -117,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATIC_URL = '/static/'  # <-- Add the leading slash ✅
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic gathers everything
+
+# Optional but useful if you have custom static assets
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# WhiteNoise storage backend (for compression + cache busting)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
